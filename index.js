babf7f908a588500676d0916fe2e5b60ffa72f5e
Gustavo <gu.losada@hotmail.com>
2022-03-12 12:26:26 -0300

    Montando URL

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-https://api.openweathermap.org/data/2.5/forecast?q=Itu&appid=
+require('dotenv').config()
 
+// const PROTOCOL = process.env.PROTOCOL
+// const APPID = process.env.APPID
+// const BASE_URL = process.env.BASE_URL
+// const LENGUAGE = process.env.LENGUAGE
+// const UNITS = process.env.UNITS
+
+//operador de desestruturação
+
+const { PROTOCOL, BASE_URL, UNITS, LANGUAGE, APPID, Q } = process.env
+
+// const url = PROTOCOL + "://" + ...
+const url = `${PROTOCOL}://${BASE_URL}?appid=${APPID}&lang=${LANGUAGE}&units=${UNITS}&q=${Q}`
+
+console.log(url)
